2df85523df2cb550f605115636f0f49abf3afa71
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-10-11 19:49:17 +0530

    added comments

--- CliCalculator.java
+++ CliCalculator.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
-
+/* 
+ * Name : Nalin Sahani 
+ * urn : 2022-M-26062001
+ */
 /**
  * CliCalculator
  */
 public class CliCalculator {
 
+    // Static method for addition
     static int add(int number1 ,int number2){
         return number1 + number2;
     }
 
+    // Static method for subtraction
     static int sub(int number1, int number2){   
         if(number2 > number1){
             System.out.println("can not substract " + number2 + " from " + number1);
@@ -16,10 +21,12 @@ public class CliCalculator {
         return number1 - number2;
     }
 
+    // Static method for multiplication
     static int multiplay(int number1, int number2){
         return number1 * number2;
     }
 
+    // Static method for division
     static int division(int number1, int number2){
 
         if(number2 == 0){
@@ -39,15 +46,19 @@ public class CliCalculator {
         String again;
 
         do {
+            // Prompt the user to enter an operation
             System.out.println("Enter an operation (+, -, *, /) : ");
             operation = scanner.next();
 
+            // Prompt the user to enter the first number
             System.out.println("Enter the first number: ");
             firstNumber = scanner.nextInt();
 
+            // Prompt the user to enter the second number
             System.out.println("Enter the second number: ");
             secondNumber = scanner.nextInt();
 
+            // Use a switch statement to determine the which operation to perform
             switch (operation.trim()) {
                 case "+":
                     result = add(firstNumber,secondNumber);
@@ -62,17 +73,20 @@ public class CliCalculator {
                     result = division(firstNumber, secondNumber);
                     break;
                 default:
+                // Inform the user of an invalid operation
                     System.out.println("Invalid operation");
                     break;
             }
+            // Display the result of the calculation
             String showResult = String.format("Result is : %d %s %d = %d", 
             firstNumber, operation, secondNumber,result);
             System.out.println(showResult);
 
+            // Ask the user if they want to perform another calculation
             System.out.println("Would you like to perform another calculation? (y/n)");
             again = scanner.next();
 
-        } while (again.equals("y"));
+        } while (again.equals("y"));//Continue the loop if the user enters 'y'
 
         scanner.close();
     }
--- README.md
+++ README.md
@@ -6,3 +6,24 @@ MCA
 ---
 
 Q 1. Create a simple command-line calculator that performs basic arithmetic operations (addition, subtraction, multiplication, division). give your logic and understanding of this particular program.
+
+## Output
+
+Enter an operation (+, -, *, /) :
++
+Enter the first number:
+20
+Enter the second number:
+30
+Result is : 20 + 30 = 50
+Would you like to perform another calculation? (y/n)
+y
+Enter an operation (+, -, *, /) :
+*
+Enter the first number:
+3455
+Enter the second number:
+233
+Result is : 3455 * 233 = 805015
+Would you like to perform another calculation? (y/n)
+n

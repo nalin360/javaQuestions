636069a54143e41c81a152803d781ff82c1f4bf1
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-10-18 20:53:08 +0530

    Reverse a string

---
+++ ReverseString.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/**
+ * ReverseString
+ */
+
+public class ReverseString {
+
+    public String revString(String str){
+        if (str.isEmpty()){
+            // If the input string is empty, return it
+            System.out.println("Empty String");
+            return str;
+        }else{
+            // Reverse the substring and append the first character
+            return revString(str.substring(1)) + str.charAt(0);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = sc1.next();
+
+        // Call the revString method to reverse the input string
+        ReverseString revobj = new ReverseString();
+        String newStr = revobj.revString(str);
+        
+        System.out.println(newStr);
+        sc1.close();
+
+    }
+}
